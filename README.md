2814a189212ead915b15dfa5fc832b85b04faca5
kanishka2790 <kanishka@iosandweb.net>
2023-04-06 12:07:44 +0530

    m

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ease Demo
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/a-trost/pen/VwaKBjP/e98b2eb0c93de1ea25e5d16a71dffdda](https://codepen.io/a-trost/pen/VwaKBjP/e98b2eb0c93de1ea25e5d16a71dffdda).
+
---
+++ dist/script.js
@@ -0,0 +1,66 @@
+gsap.to(".linear", {
+  duration: 3,
+  ease: "none",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".easeIn", {
+  duration: 3,
+  ease: "power1.in",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".easeOut", {
+  duration: 3,
+  ease: "power1.out",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+
+gsap.to(".easeInOut", {
+  duration: 3,
+  ease1: 
+  ease: "power1.inOut",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".container", {
+  duration: 3,
+  ease: "elastic.out",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".bounce", {
+  duration: 3,
+  ease: "bounce.out",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".ycontainer", {
+  duration: 3,
+  ease: "steps(6)",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".back", {
+  duration: 3,
+  ease: "back.out(1.7)",
+  x: "400px",
+  repeat: -1,
+  repeatDelay: 1
+});
+gsap.to(".back",{
+	duration:3,
+	ease:"back.out(1.7)",
+	x:"200px",
+	repeat:-1,
+	repeatDelay:1
+});
+ 
